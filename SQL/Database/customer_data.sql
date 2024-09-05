7996407104e5eb99da82412cb43d4070e5ba3e02
smileloukkade <sunicha.mah@gmail.com>
2024-09-05 17:38:33 +0700

    Update customer_data.sql
    add comma (insert value)

--- SQL/Database/customer_data.sql
+++ SQL/Database/customer_data.sql
@@ -12,7 +12,7 @@ CREATE TABLE employee (
  VALUES
     (1, 'David', 'Marketing', 'CEO', 100000),
     (2, 'John', 'Marketing', 'VP', 85000),
-    (3, 'Marry', 'Sales', 'Manager', 60000)
+    (3, 'Marry', 'Sales', 'Manager', 60000),
  	  (4, 'Harry', 'IT', 'Senior Manager', 88000),
     (5, 'Walker', 'IT', 'Manager', 68000);
 
